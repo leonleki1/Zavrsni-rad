715d8de488efe4ff649f921f1b3766426e6dcd30
leonleki1 <168353792+leonleki1@users.noreply.github.com>
2024-09-11 20:14:06 +0200

    Dodavanje pozadinske slike

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,30 @@
-import React from 'react';
-import './App.css'; // Import CSS za animaciju
+import './App.css';
+import treeLogo from '/tree.png';
 
 function App() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top" style={{ backgroundColor: 'green', padding: '10px 20px' }}>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container-fluid">
-          <div className="d-flex align-items-center w-100">
-            <a className="navbar-brand me-3" href="#" style={{ fontSize: '1.5rem', color: 'white' }}>
-              Paulovnija
+          <a className="navbar-brand" href="#">
+            Paulovnija
+          </a>
+          <img src={treeLogo} alt="Tree Logo" width="50" height="50" className="tree-logo" />
+          <a className="nav-link" href="#">O Paulovniji</a>
+          <div className="dropdown ms-auto">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Meni
             </a>
-            <img src="/tree.png" alt="Tree Logo" width="50" height="50" className="me-3 tree-logo" />
-            <a className="nav-link" href="#" style={{ color: 'white', fontSize: '1.25rem' }}>O Paulovniji</a>
-            <div className="dropdown ms-auto">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: 'white', fontSize: '1.25rem' }}>
-                Meni
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a className="dropdown-item" href="#">Radnici</a></li>
-                <li><a className="dropdown-item" href="#">Strojevi</a></li>
-                <li><a className="dropdown-item" href="#">Rasadnik</a></li>
-                <li><a className="dropdown-item" href="#">Zadaci</a></li>
-              </ul>
-            </div>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a className="dropdown-item" href="#">Radnici</a></li>
+              <li><a className="dropdown-item" href="#">Strojevi</a></li>
+              <li><a className="dropdown-item" href="#">Rasadnik</a></li>
+              <li><a className="dropdown-item" href="#">Zadaci</a></li>
+            </ul>
           </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 }
 
